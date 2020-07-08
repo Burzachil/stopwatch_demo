ef4404c8c292e37a919db9c99d444c93e4d8cdf0
Andrey Kutnyakov <Kutnyakov.99@mail.ru>
2020-07-09 00:49:50 +0300

    add build

---
+++ build/precache-manifest.9178e5cbb16618d89a6584dce1a22c6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c4288b31823381b94c8c8f16f2a6447",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c5f1f23910852229856",
+    "url": "/static/css/main.2616023a.chunk.css"
+  },
+  {
+    "revision": "8961004a6b589eb17a4c",
+    "url": "/static/js/2.e1a0f1bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e1a0f1bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c5f1f23910852229856",
+    "url": "/static/js/main.030457cd.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
